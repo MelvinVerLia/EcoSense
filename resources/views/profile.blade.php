e0d71e4f0cdcad50687b8041876409554f5b9d16
MelvinVerLia <verdinanmelvin@gmail.com>
2024-12-15 21:42:58 +0700

    why

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -148,5 +148,6 @@
 </style>
 
 
-
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js"></script>
 @endsection
